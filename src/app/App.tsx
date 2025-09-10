465b58a1ccbb6b09f61f930a0c673a22184d4362
Dumskii-Artem <boltushko@gmail.com>
2025-09-11 02:33:33 +0300

    Initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,15 @@
+// src\app\App.tsx
+
+import React from 'react';
+import { HomePage } from '../pages/HomePage';
+
+export const App = () => {
+  return (
+    <div>
+      <h1>SkillSwap</h1>
+      <HomePage />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+// src\pages\HomePage.tsx
+
+import React from 'react';
+import { Button } from '../shared/ui/Button';
+
+export const HomePage = () => {
+  return (
+    <div>
+      <h2>Главная страница</h2>
+      <Button label="Нажми меня" />
+    </div>
+  );
+};
