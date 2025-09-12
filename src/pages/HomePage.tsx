64ebe072e3068d80370b880b0aa1eb107eb2126f
Hey Looch <HeyLy4@yandex.ru>
2025-09-12 16:16:14 +0500

    feature/ButtonUI added

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,14 @@
 // src\pages\HomePage.tsx
 
 import React from 'react';
-import { Button } from '../shared/ui/Button';
+import { ButtonUI } from '../shared/ui/button/ButtonUI';
 import { Footer } from '../widgets/footer/Footer';
 
 export const HomePage = () => {
   return (
     <div>
       <h2>Главная страница</h2>
-      <Button label="Нажми меня" />
+      <ButtonUI label="UI" colored onClick={() => alert('U and I')}/>
       <Footer />
     </div>
   );
