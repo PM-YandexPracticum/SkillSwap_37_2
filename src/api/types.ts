e03533271c49dc78808103bc0a5c879c37ff6a9c
Danila Vechernin <d.vechernin03@gmail.com>
2025-09-14 17:05:26 +0300

    feature/api: added 3 api functions

---
+++ src/api/types.ts
@@ -0,0 +1,14 @@
+export type TUser = {
+  id: string;
+  name: string;
+  age: number;
+  gender: 'male' | 'female';
+  photo: string;
+  from: string;
+}
+
+export type TPlace = {
+  id: number;
+  name: string;
+}
+
