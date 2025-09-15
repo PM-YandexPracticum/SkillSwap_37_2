6aed6c1482e30557a33aaae161907159909b7b1d
Bash-Dash <daria.kuranov@gmail.com>
2025-09-15 13:38:48 +0300

    add filters

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 // src\pages\HomePage.tsx
 
-import React from "react";
+import React, { useState } from "react";
 import { ButtonUI } from "../shared/ui/button/ButtonUI";
 import { Footer } from "../widgets/footer/Footer";
 import { SkillTag } from "../features/skills/skillTag/SkillTag";
@@ -10,11 +10,28 @@ import { SkillCard } from "../features/skills/skillCard/SkillCard";
 import mockPhoto from "../../public/db/users-photo/00001.jpg";
 import { DropdownDemo } from "../widgets/dropdownDemo/DropdownDemo";
 import { AuthForm } from "../features/auth/AuthForm"; // для теста
+import { FilterSection } from "../features/filters/FilterSection";
 
 export const HomePage = () => {
+
+  const [selectedGender, setSelectedGender] = useState<string>('');
+  const [selectedCities, setSelectedCities] = useState<string[]>([]);
+
+  const handleGenderChange = (gender: string) => {
+    setSelectedGender(gender);
+  };
+  const handleCityChange = (cities: string[]) => {
+    setSelectedCities(cities);
+  };
   return (
     <>
       <Header />
+          <FilterSection
+        onGenderChange={handleGenderChange}
+        onCityChange={handleCityChange}
+        selectedGender={selectedGender}
+        selectedCities={selectedCities}
+      />
       <DropdownDemo />
       <AuthForm />
       <SkillCard
