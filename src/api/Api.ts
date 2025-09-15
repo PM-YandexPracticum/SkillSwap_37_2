a90bd908f2ca4f81b5ee651faa3c45cec42e3970
Dumskii-Artem <boltushko@gmail.com>
2025-09-15 16:44:53 +0300

    добавил новые API и теперь там есть цвета

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -43,6 +43,39 @@ export const getPlacesApi = async (): Promise<TResponsePlaces> => {
 }
 
 
+export const getSkilsApi = async (): Promise<TResponsePlaces> => {
+  try {
+    const response = await fetch('/db/skils.json')
+    const data = await response?.json()
+    return data 
+  } catch(error) {
+    console.error(error)
+    throw error
+  }
+}
+
+export const getSkilsCategoriesApi = async (): Promise<TResponsePlaces> => {
+  try {
+    const response = await fetch('/db/skils_categories.json')
+    const data = await response?.json()
+    return data 
+  } catch(error) {
+    console.error(error)
+    throw error
+  }
+}
+
+export const getSkilsSubCategoriesApi = async (): Promise<TResponsePlaces> => {
+  try {
+    const response = await fetch('/db/skils_subcategories.json')
+    const data = await response?.json()
+    return data 
+  } catch(error) {
+    console.error(error)
+    throw error
+  }
+}
+
 
 
   
