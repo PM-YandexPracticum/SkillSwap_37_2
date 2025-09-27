4cddf71e5d4474e8e78908d63bd6943d81962ac3
Hey Looch <HeyLy4@yandex.ru>
2025-09-28 03:57:44 +0500

    Revert "Merge pull request #192 from PM-YandexPracticum/feature/offerUser-reducer"
    This reverts commit ba7c9f7e7d629a4e03108c0a832accdcf99ed138, reversing
    changes made to abc7b3271a8baad15d5bd23cc37487bfd77a2000.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -58,7 +58,6 @@ import { getUsersThunk } from "../services/users/actions";
 import { getCategoriesThunk } from "../services/categories/actions";
 import { getUserLikesThunk, getUserThunk } from "../services/user/actions";
 
-import { Button } from "../shared/ui/button/Button";
 
 import { OfferPage } from "../pages/Offer/OfferPage";
 import { RegisterStep2Data } from "../features/auth/RegisterStep2";
