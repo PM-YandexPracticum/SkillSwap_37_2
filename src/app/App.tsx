9c733fd65ecd3098d9ebcd6adba7a1001f0ccc77
Hey Looch <HeyLy4@yandex.ru>
2025-09-28 20:26:51 +0500

    feat:added ScrollToTop feature

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -61,8 +61,10 @@ import { getUserLikesThunk, getUserThunk } from "../services/user/actions";
 
 import { OfferPage } from "../pages/Offer/OfferPage";
 import { RegisterStep2Data } from "../features/auth/RegisterStep2";
-import styles from "./App.module.css";
 import { getPopularUsersThunk } from "../services/popularUsers/actions";
+import { ScrollToTop } from "../features/scrollToTop/ScrollToTop";
+
+import styles from "./App.module.css";
 
 
 
@@ -91,6 +93,7 @@ export const App: React.FC = () => {
   
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         {/* Главная БЕЗ Layout, тк есть внутри фотер и хедер */}
         <Route index element={<HomePage />} />
