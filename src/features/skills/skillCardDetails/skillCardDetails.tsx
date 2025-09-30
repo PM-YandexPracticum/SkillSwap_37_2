5ad2bf077b4f336b91736b2d58ef9d0250a895d2
Hey Looch <HeyLy4@yandex.ru>
2025-09-30 22:28:09 +0500

    saved commit

--- src/features/skills/skillCardDetails/skillCardDetails.tsx
+++ src/features/skills/skillCardDetails/skillCardDetails.tsx
@@ -37,7 +37,7 @@ export const SkillCardDetails: React.FC<SkillCardDetailsProps> = ({
   const isOfferReady = useSelector(isOfferCreated);
   
   const handleExchangeClick = () => {
-    if (!requireRegistration) {
+    if (requireRegistration) {
     openNotification({
       type: "info",
       title: "Ваше предложение создано",
@@ -108,7 +108,8 @@ export const SkillCardDetails: React.FC<SkillCardDetailsProps> = ({
                 </Button>
               )}
               {isOfferReady && (
-                <Button className={styles.buttonOffer}
+                <Button
+                  className={(styles.buttonOffer, styles.buttonOfferReady)}
                   onClick={handleExchangeClick}
               >
                   <Icon name='clock'/> Обмен предложен
